555caee4ece3cd9cb1b10c2e2c76c3e1b49b8e23
yaorobin <339546210@qq.com>
2015-11-04 19:17:17 +0800

    add HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    /**
+    * Êä³öÒ»ÐÐ×Ö·û´®¡°Hello World!¡±
+    * @param args
+    */
+    public static void main(String[] args) {
+    System.out.println("Hello World!");
+    }
+}
