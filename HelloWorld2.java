3af3a0332596ebe4025cdc3498b0d7d988e95723
yaorobin <339546210@qq.com>
2015-11-05 22:31:01 +0800

    This is a new HelloWorld2.java for development

---
+++ HelloWorld2.java
@@ -0,0 +1,10 @@
+﻿public class HelloWorld {
+    /**
+    * 第一个Helloworld程序,haha
+    * @param args
+    */
+    public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println("Hello World22222!");
+    }
+}
