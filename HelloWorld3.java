2fcfeb232f9daf1e9b8ceac0c2d21eb073317fb9
yaorobin <339546210@qq.com>
2015-11-06 19:55:48 +0800

    add new file test.java change file3 in new branch

--- HelloWorld3.java
+++ HelloWorld3.java
@@ -6,5 +6,6 @@ public class HelloWorld3 {
     public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("Hello World33333!");
+    System.out.println("in new branch server branch!");
     }
 }
