332212fde96338276f0ec9f0bfcfb025b0f5f9a1
yaorobin <339546210@qq.com>
2016-03-27 16:50:11 +0800

    change 2 files, Hello4.java and HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
-public class HelloWorld {
+﻿public class HelloWorld {
     /**
     * 第一个Helloworld程序,haha
     * @param args
     */
     public static void main(String[] args) {
     System.out.println("Hello World!");
-    System.out.println("Hello World2!");
+    System.out.println("Hello World2!!!!!!");
     }
 }
