b1aa1b40e3d6c4253c42f2bc1aee2c00483692d4
yaorobin <339546210@qq.com>
2016-11-18 20:54:37 +0800

    delete some lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,9 +4,6 @@
     * @param args
     */
     public static void main(String[] args) {
-    System.out.println("Hello World!");
-    System.out.println("Hello World2!!!!!!");
-    System.out.println("Hello World33333333");
-    System.out.println("Hello World3!!!!!!");
+    System.out.println("Hello World000!");
     }
 }
